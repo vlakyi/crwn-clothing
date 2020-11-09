e9005ccca77cf09562aa8341c714b8ed7c1cc0c8
Vlad <kiyashko.vlad1@gmail.com>
2020-11-09 23:57:26 +0100

    fixing issue related to CustomButton font size

--- client/src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ client/src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -14,7 +14,8 @@ export const CartDropdownContainer = styled.div`
         right: 40px;
         z-index: 5;
         scrollbar-width: 2px;
-
+        font-size: 14px;
+        
         button {
             margin-top: auto;
         }
