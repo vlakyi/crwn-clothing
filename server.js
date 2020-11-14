7e1aaf57def482966c052f769ad498bb6437e915
Vlad <kiyashko.vlad1@gmail.com>
2020-11-14 13:57:56 +0100

    adding contact email sending functionality

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const path = require('path');
 const compression = require('compression');
 const enforce = require('express-sslify');
 
+// Email API
+const sgMail = require('@sendgrid/mail');
+
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,7 +24,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')));
     app.use(compression());
-    
+
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
@@ -46,6 +50,32 @@ app.post('/payment', (req, res) => {
             res.status(200).send({ success: stripeRes });
         }
     });
+});
+
+app.post('/contact', async (req, res) => {
+    const { name, email, message } = req.body;
+    const msg = {
+        to: 'kiyashko.vlad1@gmail.com',
+        from: email,
+        subject: `CRWN CLOTHING contact message from: ${name}`,
+        text: message
+    };
+    
+    if(email.includes('@')) {
+        try {
+            const result = await sgMail.send(msg);
+            res.status(result.statusCode).json('Email successfully sent');
+
+        } catch (error) {
+            if (error.response) {
+                console.error(error.response.body);
+                res.status(400).json(error.response.body);
+            }
+        }
+    }
+    else {
+        res.status(422).json('wrong email address format');
+    }
 });
 
 app.listen(port, error => {
