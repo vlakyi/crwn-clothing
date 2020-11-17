2462f632c725d870d76d6d1d9a7066eb84a71813
Vlad <kiyashko.vlad1@gmail.com>
2020-11-17 22:59:19 +0100

    correcting tests, adding mock for Modal component to not brake test with ReactDOM.Portal

--- client/src/__tests__/contact.test.js
+++ client/src/__tests__/contact.test.js
@@ -16,6 +16,16 @@ const server = setupServer(
     })
 );
 
+jest.mock('../components/modal/modal.component', () => ({ modalHeader, modalText, buttonText, closeModal }) => {
+    return (
+        <div>
+            <h1>{modalHeader}</h1>
+            <p>{modalText}</p>
+            <button onClick={closeModal}>{buttonText}</button>
+        </div>
+    )
+});
+
 beforeAll(() => server.listen());
 afterAll(() => server.close());
 afterEach(() => {
@@ -46,24 +56,24 @@ describe('Contact page test', () => {
 
         userEvent.type(nameInput, 'TestUser');
         await waitFor(() => expect(nameInput).toHaveValue('TestUser'));
-        
-        userEvent.type(emailInput, 'email@gmail.com');
-        await waitFor(() => expect(emailInput).toHaveValue('email@gmail.com'));
-        
-        fireEvent.change(messageTextArea, {target: {value: 'JavaScript'}});
+
+        userEvent.type(emailInput, 'test.user@gmail.com');
+        await waitFor(() => expect(emailInput).toHaveValue('test.user@gmail.com'));
+
+        fireEvent.change(messageTextArea, { target: { value: 'JavaScript' } });
         await waitFor(() => expect(messageTextArea).toHaveValue('JavaScript'));
 
         userEvent.click(submitButton);
 
-        await waitFor( () => { expect(nameInput).toHaveValue('') });
-        await waitFor( () => { expect(emailInput).toHaveValue('') });
-        await waitFor( () => { expect(messageTextArea).toHaveValue('') });
-        
+        await waitFor(() => { expect(nameInput).toHaveValue('') });
+        await waitFor(() => { expect(emailInput).toHaveValue('') });
+        await waitFor(() => { expect(messageTextArea).toHaveValue('') });
+
         expect(screen.getByText('Success').textContent).toEqual('Success');
-        
+
         userEvent.click(screen.getByText('Close'));
-        
-        await waitFor( () => { expect(screen.queryByText('Success')).not.toBeInTheDocument(); });
+
+        await waitFor(() => { expect(screen.queryByText('Success')).not.toBeInTheDocument(); });
     });
 
     it('Test modal open and close on empty form submition', async () => {
@@ -73,11 +83,11 @@ describe('Contact page test', () => {
 
         userEvent.click(submitButton);
 
-        await waitFor( () => { expect(screen.getByText('Wrong credentials').textContent).toEqual('Wrong credentials')});
-        
+        await waitFor(() => { expect(screen.getByText('Wrong credentials').textContent).toEqual('Wrong credentials') });
+
         userEvent.click(screen.getByText('OK'));
-        
-        await waitFor( () => { expect(screen.queryByText('Wrong credentials')).not.toBeInTheDocument(); });
+
+        await waitFor(() => { expect(screen.queryByText('Wrong credentials')).not.toBeInTheDocument(); });
     });
 
 });
