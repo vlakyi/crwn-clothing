6572d8be73d4e158aa57ec2829e324442a5115bd
Vlad <kiyashko.vlad1@gmail.com>
2020-11-23 23:33:41 +0100

    Adding modal component to stripe-button component after payment success or fail

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import axios from 'axios';
+import Modal from '../modal/modal.component';
+import useModal from '../../hooks/useModal/useModal';
 
 const StripeCheckoutButton = ({ price }) => {
+    // Modal config
+    const [modalState, dispatchModal] = useModal();
+    const { isDisplay, isSuccess, modalHeader, modalText, buttonText } = modalState;
+
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51HbTj5LwL05sOU5NX1aSwhfgOEfKnbxfbyPoAqTZ1N5UO2vRAODLyipOiwyG7ND4kQG13F5lJAqlkVQcWhPUUuGM00Ekb8Tj2D';
 
@@ -16,28 +22,44 @@ const StripeCheckoutButton = ({ price }) => {
                 token
             }
         })
-        .then(response => {
-            alert('Payment Successful');
-        })
-        .catch(error => {
-            console.log('Payment error: ', JSON.parse(error));
-            alert('There was an issue with your payment. Please make sure you use the provided credit card.');
-        });
+            .then(response => {
+                dispatchModal({
+                    type: 'openModal', payload: {
+                        isSuccess: true,
+                        modalHeader: 'Payment Successful',
+                        modalText: 'Thank you for the order :)',
+                        buttonText: 'CLOSE'
+                    }
+                });
+            })
+            .catch(error => {
+                console.log('Payment error: ', JSON.parse(error));
+                dispatchModal({
+                    type: 'openModal', payload: {
+                        modalHeader: 'Payment error',
+                        modalText: 'There was an issue with your payment. Please make sure you use the provided credit card.',
+                        buttonText: 'OK'
+                    }
+                });
+            });
     }
 
     return (
-        <StripeCheckout
-            label="Pay Now"
-            name="CRWN Clothing Ltd."
-            billingAddress
-            shippingAddress
-            image='https://svgshare.com/i/CUz.svg'
-            description={`Your total is $${price}`}
-            amount={priceForStripe}
-            panelLabel='Pay Now'
-            token={onToken}
-            stripeKey={publishableKey}
-        />
+        <>
+            <StripeCheckout
+                label="Pay Now"
+                name="CRWN Clothing Ltd."
+                billingAddress
+                shippingAddress
+                image='https://svgshare.com/i/CUz.svg'
+                description={`Your total is $${price}`}
+                amount={priceForStripe}
+                panelLabel='Pay Now'
+                token={onToken}
+                stripeKey={publishableKey}
+            />
+            {isDisplay && <Modal closeModal={() => dispatchModal({ type: 'closeModal' })} modalHeader={modalHeader} modalText={modalText} buttonText={buttonText} isSuccess={isSuccess} />}
+        </>
     );
 };
 
