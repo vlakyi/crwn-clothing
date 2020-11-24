f4a10776db2a719723c6b7987ca16df6fb0c4c95
Vlad <kiyashko.vlad1@gmail.com>
2020-11-24 23:16:28 +0100

    redesigning collection-item component and fixing responsiveness issue in collection component

--- client/src/components/collection-item/collection-item.component.jsx
+++ client/src/components/collection-item/collection-item.component.jsx
@@ -4,17 +4,17 @@ import { connect } from 'react-redux';
 import { addItem } from '../../redux/cart/cart.actions';
 
 import {CollectionItemContainer, BackgroundImage, CollectionFooterContainer, NameContainer, PriceContainer, AddButton} from './collection-item.styles';
-
 const CollectionItem = ({ item, addItem }) => {
     const { imageUrl, name, price } = item;
+
     return (
         <CollectionItemContainer>
+            <AddButton onClick={() => addItem(item)}></AddButton>
             <BackgroundImage imageUrl={imageUrl}/>
             <CollectionFooterContainer>
                 <NameContainer>{name}</NameContainer>
-                <PriceContainer>${price}</PriceContainer>
+                <PriceContainer>{price.toFixed(2)}$</PriceContainer>
             </CollectionFooterContainer>
-            <AddButton onClick={() => addItem(item)} inverted>Add to cart</AddButton>
         </CollectionItemContainer>
     )
 };
--- client/src/pages/collection/collection.styles.jsx
+++ client/src/pages/collection/collection.styles.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const CollectionPageContainer = styled.div`
     display: flex;
+    align-items: center;
     flex-direction: column;
     margin-top: 100px;
 `;
@@ -9,10 +10,20 @@ export const CollectionPageContainer = styled.div`
 export const ItemsContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-gap: 10px;
+  grid-gap: 30px;
 
   & > div {
     margin-bottom: 30px;
+  }
+
+  @media screen and (min-width: 1800px) {
+    display: flex;
+    gap: 40px;
+    flex-wrap: wrap;
+  }
+
+  @media screen and (max-width:1024px) {
+    grid-template-columns: 1fr 1fr 1fr;
   }
 
   @media screen and (max-width:800px) {
