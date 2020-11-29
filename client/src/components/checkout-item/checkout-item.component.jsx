4d15ddcc420829354a307c0a750325e2b4e2ab96
Vlad <kiyashko.vlad1@gmail.com>
2020-11-29 21:23:18 +0100

    Redesigning checkout component

--- client/src/components/checkout-item/checkout-item.component.jsx
+++ client/src/components/checkout-item/checkout-item.component.jsx
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import Add from '@material-ui/icons/AddRounded';
+import Remove from '@material-ui/icons/RemoveRounded';
+
 
 import { connect } from 'react-redux';
 import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
 
-import { CheckoutItemContainer, ImageContainer, Name, Quantity, Price, Arrow, Value, RemoveButton } from './checkout-item.styles';
+import { CheckoutItemContainer, ImageContainer, CheckoutInfoWrapper, CheckoutInfoContainer, Name, QuantityContainer, Quantity, Price, RemoveButton } from './checkout-item.styles';
+import { throttle } from 'lodash';
 
 const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
     const { name, imageUrl, price, quantity } = cartItem;
+    const imageRef = useRef();
+    const [imageHeight, setImageHeight] = useState('');
+
+    const handleWindowResize = throttle(() => {
+        setImageHeight(imageRef.current.offsetHeight);
+    }, 150);
+
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => window.removeEventListener('resize', handleWindowResize);
+
+    }, [handleWindowResize]);
+
     return (
         <CheckoutItemContainer>
             <ImageContainer>
-                <img src={imageUrl} alt="item" />
+                <img ref={imageRef} src={imageUrl} onLoad={handleWindowResize} alt="item" />
             </ImageContainer>
-            <Name>{name}</Name>
-            <Quantity>
-                <Arrow onClick={() => removeItem(cartItem)}>&#10094;</Arrow>
-                <Value>{quantity}</Value>
-                <Arrow onClick={() => addItem(cartItem)}>&#10095;</Arrow>
-            </Quantity>
-            <Price>${price}</Price>
-            <RemoveButton onClick={() => clearItem(cartItem)}>&#10005;</RemoveButton>
+
+            <CheckoutInfoWrapper height={imageHeight}>
+                <CheckoutInfoContainer>
+                    <Name>{name}</Name>
+                    <Price>${price.toFixed(2)}</Price>
+                    <RemoveButton style={{ fontSize: 32 }} onClick={() => clearItem(cartItem)} />
+                </CheckoutInfoContainer>
+
+                <QuantityContainer>
+                    <Add onClick={() => addItem(cartItem)} />
+                    <Quantity>{quantity}</Quantity>
+                    <Remove onClick={() => removeItem(cartItem)} />
+                </QuantityContainer>
+            </CheckoutInfoWrapper>
+
         </CheckoutItemContainer>
     )
 };
--- client/src/components/checkout-item/checkout-item.styles.jsx
+++ client/src/components/checkout-item/checkout-item.styles.jsx
@@ -1,4 +1,5 @@
-import styled, {css} from 'styled-components';
+import styled, { css } from 'styled-components';
+import Delete from '@material-ui/icons/Delete';
 
 const defaultWidth = css`
     width: 23%;
@@ -7,47 +8,111 @@ const defaultWidth = css`
 export const CheckoutItemContainer = styled.div`
     width: 100%;
     display: flex;
+    align-items: flex-start;
     min-height: 100px;
     border-bottom: 1px solid darkgrey;
     padding: 15px 0;
-    font-size: 20px;
-    align-items: center;
+    position: relative;
 `;
 
 export const ImageContainer = styled.div`
-    width: 23%;
-    padding-right: 15px;
+    width: 30vw;
+    min-width: 110px;
+    min-height: 120px;
+    max-width: 300px;
+    margin-right: 20px;
 
     img {
-    width: 100%;
-    height: 100%;
+        width: 100%;
+        height: 100%;
+        border-radius: 16px;
     }
 `;
 
-export const Name = styled.span`
-    ${defaultWidth};
+export const CheckoutInfoWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: ${({height}) => height ? height + 'px' : 'auto'};
+`;
+
+export const CheckoutInfoContainer = styled.div`
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    align-self: flex-start;
+
+    @media screen and (min-width: 360px) {
+        font-size: 18px;
+    }
+
+    @media screen and (min-width: 400px) {
+        font-size: 20px;
+    }
+
+    @media screen and (min-width: 400px) {
+        font-size: 22px;
+    }
+`;
+
+export const Name = styled.h4`
+    margin: 0px;
+    margin-bottom: 12px;
+    font-weight: 600;
+`;
+
+export const QuantityContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-self: center;
+    height: 100px;
+    margin-left: 15px;
+    box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.16);
+    padding: 5px;
+    border-radius: 32px;
+    font-size: 16px;
+    font-weight: 600;
+
+    & :first-child, :last-child {
+        cursor: pointer;
+    }
+
+    & svg {
+        color: #9e9d9d;
+    }
+
+    @media screen and (min-width: 768px) {
+        font-size: 18px;
+        height: 125px;
+    }
+
+    @media screen and (min-width: 1024px) {
+        font-size: 22px;
+        height: 150px;
+
+        & svg {
+            font-size: 36px;
+        }
+    }
 `;
 
 export const Quantity = styled.span`
-    ${defaultWidth};
-    display: flex;
-    margin: 0 15px;
+     margin: 0 auto;
 `;
 
 export const Price = styled.span`
-    ${defaultWidth};
+     ${defaultWidth};
 `;
 
-export const Arrow = styled.div`
+export const RemoveButton = styled(Delete)`
     cursor: pointer;
-`;
+    position: absolute;
+    bottom: 15px;
+    margin-left: -5px;
 
-export const Value = styled.span`
-     margin: 0 10px;
+    & :hover {
+        fill: rgb(196, 53, 53);
+    }
 `;
-
-export const RemoveButton = styled.div`
-    padding-left: 12px;
-    cursor: pointer;
-`;
-  
