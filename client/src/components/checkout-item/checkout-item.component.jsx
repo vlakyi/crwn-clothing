2de47acfaee14d923f23a1df608574c68bac9905
Vlad <kiyashko.vlad1@gmail.com>
2020-11-30 21:31:39 +0100

    Adding changing quantity value by writing it on keyboard functonality

--- client/src/components/checkout-item/checkout-item.component.jsx
+++ client/src/components/checkout-item/checkout-item.component.jsx
@@ -2,27 +2,54 @@ import React, { useRef, useEffect, useState } from 'react';
 import Add from '@material-ui/icons/AddRounded';
 import Remove from '@material-ui/icons/RemoveRounded';
 
-
-import { connect } from 'react-redux';
-import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
-
-import { CheckoutItemContainer, ImageContainer, CheckoutInfoWrapper, CheckoutInfoContainer, Name, QuantityContainer, Quantity, Price, RemoveButton } from './checkout-item.styles';
 import { throttle } from 'lodash';
+import { connect } from 'react-redux';
+import {
+    clearItemFromCart,
+    addItem,
+    setItemQuantity,
+    removeItem
+} from '../../redux/cart/cart.actions';
 
-const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+import {
+    CheckoutItemContainer,
+    ImageContainer,
+    CheckoutInfoWrapper,
+    CheckoutInfoContainer,
+    Name,
+    QuantityContainer,
+    Quantity,
+    Price,
+    RemoveButton
+} from './checkout-item.styles';
+
+const CheckoutItem = ({ cartItem, clearItem, addItem, setItemQuantity, removeItem }) => {
     const { name, imageUrl, price, quantity } = cartItem;
     const imageRef = useRef();
     const [imageHeight, setImageHeight] = useState('');
+    const [cartItemQuantity, setCartItemQuantity] = useState(quantity);
 
     const handleWindowResize = throttle(() => {
-        setImageHeight(imageRef.current.offsetHeight);
+        setImageHeight(`${imageRef.current.offsetHeight}px`);
     }, 150);
+
+    const handleQuantityChange = (e) => {
+        setCartItemQuantity(e.target.value);
+    };
+
+    const handleInputBlur = (e) => {
+        let quantity = e.target.value === '' ? 0 : parseInt(e.target.value);
+        setItemQuantity({item: cartItem, quantity});
+    }
+
+    useEffect(() => {
+        setCartItemQuantity(quantity);
+    }, [quantity]);
 
     useEffect(() => {
         window.addEventListener('resize', handleWindowResize);
 
         return () => window.removeEventListener('resize', handleWindowResize);
-
     }, [handleWindowResize]);
 
     return (
@@ -40,19 +67,24 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
 
                 <QuantityContainer>
                     <Add onClick={() => addItem(cartItem)} />
-                    <Quantity>{quantity}</Quantity>
+                    <Quantity
+                        type="number"
+                        value={cartItemQuantity}
+                        onChange={handleQuantityChange}
+                        onBlur={handleInputBlur}
+                    />
                     <Remove onClick={() => removeItem(cartItem)} />
                 </QuantityContainer>
             </CheckoutInfoWrapper>
-
         </CheckoutItemContainer>
-    )
+    );
 };
 
-const mapDispatchToProps = dispatch => ({
-    clearItem: item => dispatch(clearItemFromCart(item)),
-    addItem: item => dispatch(addItem(item)),
-    removeItem: item => dispatch(removeItem(item))
-})
+const mapDispatchToProps = (dispatch) => ({
+    clearItem: (item) => dispatch(clearItemFromCart(item)),
+    addItem: (item) => dispatch(addItem(item)),
+    setItemQuantity: (itemAndQuantity) => dispatch(setItemQuantity(itemAndQuantity)),
+    removeItem: (item) => dispatch(removeItem(item))
+});
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
