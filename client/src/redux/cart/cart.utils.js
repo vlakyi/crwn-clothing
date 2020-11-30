0a6bf956c1990277a6cc7dc656641b12b8f45f6b
Vlad <kiyashko.vlad1@gmail.com>
2020-11-30 21:18:22 +0100

    merging addItemToCart, removeItemFromCart inside setItemQuantity function

--- client/src/redux/cart/cart.utils.js
+++ client/src/redux/cart/cart.utils.js
@@ -1,27 +1,43 @@
-export const addItemToCart = (cartItems, cartItemToAdd) => {
-    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
+import CartActionTypes from './cart.types';
 
-    if (existingCartItem) {
-        return cartItems.map(cartItem =>
-            cartItem.id === cartItemToAdd.id
-                ? { ...cartItem, quantity: cartItem.quantity + 1 }
-                : cartItem
-        );
-    }
-    return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
-}
+const { ADD_ITEM, REMOVE_ITEM, SET_ITEM_QUANTITY } = CartActionTypes;
 
-export const removeItemFromCart = (cartItems, cartItemToRemove) => {
-    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
+export const setItemQuantity = (actionType, cartItems, cartItemToModify, quantity) => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToModify.id);
+    switch (actionType) {
+        case ADD_ITEM:
+            if (existingCartItem) {
+                return cartItems.map(cartItem => {
+                    if (cartItem.id === cartItemToModify.id)
+                        return { ...cartItem, quantity: cartItem.quantity + 1 }
+                    return cartItem;
+                });
+            }
+            return [...cartItems, { ...cartItemToModify, quantity: 1 }];
 
-    if (existingCartItem.quantity === 1) {
-        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
-    }
-    else {
-        return cartItems.map(cartItem =>
-            cartItem.id === cartItemToRemove.id
-                ? { ...cartItem, quantity: cartItem.quantity - 1 }
-                : cartItem
-        );
+        case REMOVE_ITEM:
+            if (existingCartItem && existingCartItem.quantity > 1)
+                return cartItems.map(cartItem => {
+                    if (cartItem.id === cartItemToModify.id)
+                        return { ...cartItem, quantity: cartItem.quantity - 1 }
+                    return cartItem;
+                });
+
+            return cartItems.filter(cartItem => cartItem.id !== cartItemToModify.id)
+
+        case SET_ITEM_QUANTITY:
+            if (existingCartItem && quantity > 0)
+                return cartItems.map(cartItem => {
+                    if (cartItem.id === cartItemToModify.id)
+                        return { ...cartItem, quantity: quantity }
+                    return cartItem;
+                });
+            else if (existingCartItem && quantity <= 0)
+                return cartItems.filter(cartItem => cartItem.id !== cartItemToModify.id)
+            else
+                return [...cartItems, { ...cartItemToModify, quantity: quantity }]
+
+        default:
+            return cartItems;
     }
 }
